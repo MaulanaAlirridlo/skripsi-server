dbdb48c398051ac166525c78d4d0d0f173b956b5
Maulana Alirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2023-05-24 12:33:27 +0700

    Mempersingkat kode

--- bagOfWords.py
+++ bagOfWords.py
@@ -4,4 +4,4 @@ import numpy as np
 def bowProcess(tweetList):
     vectorizer = CountVectorizer()
     bow = vectorizer.fit_transform(tweetList).todense()
-    return np.array(bow, dtype=object).tolist()
+    return bow.tolist()
--- tfIdf.py
+++ tfIdf.py
@@ -4,4 +4,4 @@ import numpy as np
 def tfIdfProcess(tweetList):
     tfIdfVectorizer = TfidfVectorizer(use_idf=True)
     tfIdf = tfIdfVectorizer.fit_transform(tweetList).todense()
-    return np.array(tfIdf, dtype=object).tolist()
+    return tfIdf.tolist()
